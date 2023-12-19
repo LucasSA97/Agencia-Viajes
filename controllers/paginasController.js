142cdd9d02ca5c2bb176ea6736148f3003fa2b95
Lucas <lucassarachu7@gmail.com>
2023-12-19 17:34:49 +0100

    Pagina controller y routes

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,34 +1,36 @@
-import { Viaje } from '../models/Viaje.js';
+import { Viaje } from "../models/Viajes.js";
 // import { Testimonios } from '../models/Testimonios.js';
 
 const paginaInicio = async (req, res) => {
-    
-    const promises = [];
+    res.render('inicio', {
+        pagina: 'Inicio'
+    })
+    // const promises = [];
 
-    promises.push(Viaje.findAll({
-        limit: 3
-    }));
+    // promises.push(Viaje.findAll({
+    //     limit: 3
+    // }));
 
-    promises.push(Testimonial.findAll({
-        limit: 3
-    }));
+    // promises.push(Testimonial.findAll({
+    //     limit: 3
+    // }));
 
 
-    try {
-        // pasar al promise
-        const resultado =  await Promise.all(promises);
+    // try {
+    //     // pasar al promise
+    //     const resultado =  await Promise.all(promises);
 
-        console.log(resultado[0])
+    //     console.log(resultado[0])
 
-        res.render('inicio', {
-            viajes : resultado[0],
-            testimoniales: resultado[1],
-            clase : 'home',
-            page: 'Inicio',
-        })
-    } catch (error) {
-        console.log(error);
-    }
+    //     res.render('inicio', {
+    //         viajes : resultado[0],
+    //         testimoniales: resultado[1],
+    //         clase : 'home',
+    //         page: 'Inicio',
+    //     })
+    // } catch (error) {
+    //     console.log(error);
+    // }
 
 }
 
@@ -46,6 +48,7 @@ const paginaTestimonios = (req, res) => {
 const paginaViajes = async  (req, res) => { 
     // Consultar BD 
     const viajes = await Viaje.findAll();
+    
     
     res.render('viajes', {
         pagina: 'Próximos Viajes', 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import express  from "express";
-import { paginaInicio, paginaTestimonios, paginaViajes, paginaNosotros } from "../controllers/paginasController";
-
+import { paginaInicio, paginaTestimonios, paginaViajes, paginaNosotros } from "../controllers/paginasController.js";
 
 const router = express.Router()
 
